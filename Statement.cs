6cd6d79636e9983c0eaca4bd6c45d0f5fec5aeb1
ThunderCrestt <50324506+ThunderCrestt@users.noreply.github.com>
2019-05-06 15:26:32 +0200

    Add files via upload
    La classe Statement qui est plutôt simple <3

---
+++ Statement.cs
@@ -0,0 +1,49 @@
+using System;
+using K3;
+namespace K3 
+{
+
+
+class Statement
+{
+	private string _statement;
+	private string _source;
+	private double _data;
+	private bool _mark;
+
+	public string statement
+	{
+		get { return _statement; }
+		set { _statement = value;}
+	}
+
+	public string source
+	{
+		get { return _source;}
+		set { _source=value;}
+	}
+
+	public double data
+	{
+		get { return _data; }
+		set { _data = value; }
+	}
+
+	public bool mark
+	{
+		get { return _mark;}
+		set {_mark=value;}
+	}
+
+	public Statement(double d, string s1,string s2,bool b=false)
+	{
+		_data=d;
+		_statement=s1;
+		_source=s2;
+		_mark=b;
+	}
+
+}
+
+
+}
