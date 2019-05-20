4faf04d143ad89a0cfaceba14a2cc18955820d63
IsterXable <50324613+IsterXable@users.noreply.github.com>
2019-05-20 13:51:02 +0200

    Add files via upload

---
+++ TexteInterface.cs
@@ -0,0 +1,42 @@
+﻿#define DEBUG
+
+using System;
+namespace TEST
+{
+    public class TexteInterface //: Interface
+    {
+        //attributs
+
+        //Constructeur
+        public TexteInterface()
+        {
+            //Interface(); //Appel au constructeur de la classe mère.
+        }
+
+        //Getteur et setteur
+
+        //Méthodes
+#if (DEBUG)
+        public void displayStratement()
+        {
+            Console.Write("%s avec %d", "la population française : ", 65000000);
+        }
+#else
+    public void displayStatement(Statement statement)
+    {
+        Console.Write("%s avec %d", "la population française : "/*statement.getStatement()*/, 65000000/*statement.getData*/);
+    }
+#endif
+
+        public void displayGame(/*PlusMinus plusMinus*/)
+        {
+            Console.Write(", versus , c'est plus ou c'est moins ? : )");
+
+        }
+/*
+        public void displayScore(PlusMinus plusMinus)
+        {
+            //A compléter
+        }*/
+    }
+}
