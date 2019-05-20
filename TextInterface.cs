2ff7297561bf4e2f66b738715aecde3e52232144
IsterXable <50324613+IsterXable@users.noreply.github.com>
2019-05-20 14:18:53 +0200

    Add files via upload

---
+++ TextInterface.cs
@@ -0,0 +1,37 @@
+﻿#define DEBUG
+
+using System;
+namespace TEST
+{
+    public class TexteInterface //: Interface
+    {
+        //attributs
+
+        //Constructeur
+        public TexteInterface()
+        {
+            //Interface(); //Appel au constructeur de la classe mère.
+        }
+
+        //Getteur et setteur
+
+        //Méthodes
+        public void displayStatement(Statement[] statements)
+        {
+            Console.WriteLine("{0} avec {1}", statements[0].statement, statements[0].data);
+            Console.WriteLine("A votre avis, c'est plus ou c'est moins que :");
+            Console.WriteLine("{0} avec {1} ?", statements[1].statement, statements[1].data);
+        }
+
+        public void displayGame(PlusMinus plusMinus)
+        {
+            Console.WriteLine(plusMinus.rules);
+
+        }
+
+        public void displayScore(PlusMinus plusMinus)
+        {
+            Console.WriteLine("score :{0}",plusMinus.score);
+        }
+    }
+}
